545e060170414ee0f8f7e53f680b4ada33449d94
Luke Matheny <lukem17@gmail.com>
2017-11-17 00:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+var objMod = object;
+delete objMod[key]
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
