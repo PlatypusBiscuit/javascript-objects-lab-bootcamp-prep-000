28fa565a6632b8fe7314a098cbc20665cefc378d
Luke Matheny <lukem17@gmail.com>
2017-11-17 00:59:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-Object.assign(object, key, value)
+  var objMod = Object.assign({}, object);
+  Object.assign(object, key, value);
+  return objMod;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-Object.assign(object, key, value)
+  Object.assign(object, key, value)
 }
 
 function deleteFromObjectByKey(object, key) {
-var objMod = Object.assign({}, object);
-delete objMod[key];
-return objMod;
+  var objMod = Object.assign({}, object);
+  delete objMod[key];
+  return objMod;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
